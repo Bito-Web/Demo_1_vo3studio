afd994c4f405e58c87703fbddb22201974c88544
Jorge Torrelles <70519224+Bito-Web@users.noreply.github.com>
2022-02-23 11:17:16 -0300

    Add Fqas section

--- js/preloader.js
+++ js/preloader.js
@@ -1,48 +1,67 @@
-// window.addEventListener("load", () => {
+window.addEventListener("load", () => {
 //     document.querySelector(".preloader").style.display = "none";
 //     document.querySelectorAll(".load").style.display = "";
 //     document.querySelectorAll(".load").style.visibility = "visible";
-// })
 
-var img = document.querySelectorAll(".img");
-var img_link = document.querySelectorAll(".img_link");
-var header = document.querySelector(".header");
+    var img = document.querySelectorAll(".img");
+    var img_link = document.querySelectorAll(".img_link");
+    var header = document.querySelector(".header");
+    var section_5_item_title = document.querySelectorAll('.section_5__item__title');
+    var section_5_item_content = document.querySelectorAll('.section_5__item__content');
+    var fas = document.querySelectorAll('.fas .fa-chevron-down');
 
-window.addEventListener("scroll", () => {
-    var y = window.scrollY;
-    var mobile = Boolean (window.innerWidth <= 768 ? true : false);
-    console.log(mobile);
-    if(mobile) {
-        if (y >= 300) {
-            header.style.backgroundColor = "rgba(0,0,0,0.5)";
+    // Change style with scroll
+    window.addEventListener("scroll", () => {
+        var y = window.scrollY;
+        var mobile = Boolean (window.innerWidth <= 768 ? true : false);
+        console.log(mobile);
+        if(mobile) {
+            if (y >= 300) {
+                header.style.backgroundColor = "rgba(0,0,0,0.5)";
+            }else {
+                header.style.backgroundColor = "rgba(0,0,0,0)";
+            }
         }else {
-            header.style.backgroundColor = "rgba(0,0,0,0)";
-        }
-    }else {
-        if (y >= 500) {
-            header.style.backgroundColor = "rgba(0,0,0,0.5)";
-        }else {
-            header.style.backgroundColor = "rgba(0,0,0,0)";
-        }
+            if (y >= 500) {
+                header.style.backgroundColor = "rgba(0,0,0,0.5)";
+            }else {
+                header.style.backgroundColor = "rgba(0,0,0,0)";
+            }
 
-    }
-});
+        }
+    });
+    // Show link in img with hover
+    img.forEach((element, index) => {
+        element.addEventListener("mouseenter", (e) => {
+            e.preventDefault();
+            img_link[index].classList.add("show");
+        });
+        element.addEventListener("mouseleave", (e) => {
+            e.preventDefault();
+            img_link[index].classList.remove("show");
+        });
+        element.addEventListener("touchstart", (e) => {
+            e.preventDefault();
+            img_link[index].classList.add("show");
+        });
+        element.addEventListener("touchend", (e) => {
+            e.preventDefault();
+            img_link[index].classList.remove("show");
+        });
+    });
+    // Open and close Faqs
+    section_5_item_title.forEach((element, index) => {
+        element.addEventListener('click', (e) => {
+            e.preventDefault();
+            section_5_item_content[index].classList.toggle('showFaqs');
 
-img.forEach((element, index) => {
-    element.addEventListener("mouseenter", (e) => {
-        e.preventDefault();
-        img_link[index].classList.add("show");
-    });
-    element.addEventListener("mouseleave", (e) => {
-        e.preventDefault();
-        img_link[index].classList.remove("show");
-    });
-    element.addEventListener("touchstart", (e) => {
-        e.preventDefault();
-        img_link[index].classList.add("show");
-    });
-    element.addEventListener("touchend", (e) => {
-        e.preventDefault();
-        img_link[index].classList.remove("show");
+            if (section_5_item_content[index].classList.contains('showFaqs')) {
+                fas[index].classList.add('fa-chevron-up');
+                fas[index].classList.remove('fa-chevron-down');
+            }else {
+                fas[index].classList.add('fa-chevron-down');
+                fas[index].classList.remove('fa-chevron-up');
+            }
+        });
     });
 });
