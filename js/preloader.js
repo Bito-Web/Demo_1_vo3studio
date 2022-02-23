04b2715110e1dd52e805ba9863fa39ca7ae810c8
Jorge Torrelles <70519224+Bito-Web@users.noreply.github.com>
2022-02-23 10:58:20 -0300

    3er Commit fix scroll styles

--- js/preloader.js
+++ js/preloader.js
@@ -1,20 +1,48 @@
-// window.addEventListener('load', () => {
-//     document.querySelector('.preloader').style.display = 'none';
-//     document.querySelectorAll('.load').style.display = '';
-//     document.querySelectorAll('.load').style.visibility = 'visible';
+// window.addEventListener("load", () => {
+//     document.querySelector(".preloader").style.display = "none";
+//     document.querySelectorAll(".load").style.display = "";
+//     document.querySelectorAll(".load").style.visibility = "visible";
 // })
 
-var img = document.querySelectorAll('.img');
-var img_link = document.querySelectorAll('.img_link');
+var img = document.querySelectorAll(".img");
+var img_link = document.querySelectorAll(".img_link");
+var header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+    var y = window.scrollY;
+    var mobile = Boolean (window.innerWidth <= 768 ? true : false);
+    console.log(mobile);
+    if(mobile) {
+        if (y >= 300) {
+            header.style.backgroundColor = "rgba(0,0,0,0.5)";
+        }else {
+            header.style.backgroundColor = "rgba(0,0,0,0)";
+        }
+    }else {
+        if (y >= 500) {
+            header.style.backgroundColor = "rgba(0,0,0,0.5)";
+        }else {
+            header.style.backgroundColor = "rgba(0,0,0,0)";
+        }
+
+    }
+});
 
 img.forEach((element, index) => {
-    element.addEventListener('mouseenter', (e) => {
+    element.addEventListener("mouseenter", (e) => {
         e.preventDefault();
-        img_link[index].classList.add('show');
+        img_link[index].classList.add("show");
     });
-    element.addEventListener('mouseleave', (e) => {
+    element.addEventListener("mouseleave", (e) => {
         e.preventDefault();
-        img_link[index].classList.remove('show');
+        img_link[index].classList.remove("show");
     });
-
+    element.addEventListener("touchstart", (e) => {
+        e.preventDefault();
+        img_link[index].classList.add("show");
+    });
+    element.addEventListener("touchend", (e) => {
+        e.preventDefault();
+        img_link[index].classList.remove("show");
+    });
 });
