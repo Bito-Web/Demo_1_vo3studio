1719fe29e0d9d7e7d540d2b5c18e8b2e20bd0b80
Jorge Torrelles <70519224+Bito-Web@users.noreply.github.com>
2022-02-24 19:01:05 -0300

    Cleaning code and styles

--- js/slick.js
+++ js/slick.js
@@ -2,17 +2,18 @@
 $(document).ready(function(){
     $('.section_5__items').slick({
         slidesToShow: 4,
-        centerPadding: '12.5%',
-        centerMode: true,
-        dots: false,
+        slidesToScroll: 1,
+        centerPadding: '0px',
+        centerMode: false,
+        dots: true,
         arrows: true,
-        infinite: true,
+        infinite: false,
         responsive: [
             {
                 breakpoint: 1920,
                 settings: {
                     slidesToShow: 3,
-                    dots: false,
+                    dots: true,
                     arrows: true
                 }
             },
@@ -20,7 +21,7 @@ $(document).ready(function(){
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 2,
-                    dots: false,
+                    dots: true,
                     arrows: true
                 }
             },
@@ -28,8 +29,10 @@ $(document).ready(function(){
                 breakpoint: 768,
                 settings: {
                     slidesToShow: 1,
+                    centerMode: true,
                     dots: true,
-                    arrows: false
+                    arrows: false,
+                    centerPadding: '20px',
                 }
             }
         ]
